f0bb51e8f0486e4a9736ce03361867867bd0699c
max-student <max-student@maxtrain.com>
2020-02-11 08:29:28 -0500

    Initial Commit

---
+++ StringstutorialProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringstutorialProject {
+    class Program {
+        static void Main(string[] args) {
+           // Console.WriteLine("Hello World!");
+            var st1 = new StringsTutorial();
+            var fullname = st1.Fullname();
+
+            Console.WriteLine($"Fullname is {fullname}");
+            Console.WriteLine($"Fullname is {st1.Fullname2()}");
+
+            var denise = new StringsTutorial("Denise", "Bartick");
+            Console.WriteLine($"Denise {denise.Fullname()}");
+            Console.WriteLine($"Denise {denise.Fullname2()}");
+
+
+            }
+
+        }
+    }
---
+++ StringstutorialProject/StringsTutorial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringstutorialProject {
+    class StringsTutorial {
+        //Properties
+        public string Firstname; //These are now properties in the method, the only function/exist in this method
+        public string Lastname;
+
+        //Special Method called Constructor,(name has to be class name) used to give class instance data, doesn't 
+        public StringsTutorial() {
+            Firstname = "George";
+            Lastname = "Meyer";
+            }
+        public StringsTutorial(string firstname, string lastname) { //first name in constructor is a parameter, Firstname below is a property!
+            Firstname = firstname;
+            Lastname = lastname;
+            }
+
+
+        //Methods
+        public string Fullname() { //First method example
+            //var Firstname = "George";
+            //var Lastname = "Meyer";
+
+            // var fullname = Firstname + " " + Lastname;
+            var fullname = $"{Lastname}, {Firstname}";
+            return fullname;
+            }
+        public string Fullname2() {//Second method example
+            //var Firstname = "George";
+            //var Lastname = "Meyer";
+
+            var fullname = $"{Firstname} {Lastname}";
+            return fullname;
+            }
+        }
+    }
